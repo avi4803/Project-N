13a89492e12a61c926e507374921e708b3d74473
Avinash Nishad <avinashn157@outlook.com>
2026-02-09 03:19:30 +0530

    Testing actions

--- main-backend/src/controllers/info-controller.js
+++ main-backend/src/controllers/info-controller.js
@@ -3,7 +3,7 @@ const { StatusCodes } = require('http-status-codes');
 const info = (req, res) => {
     return res.status(StatusCodes.OK).json({
         success: true,
-        message: 'API is live againnnn',
+        message: 'API is live(checking github actions)',
         error: {},
         data: {},
     });
