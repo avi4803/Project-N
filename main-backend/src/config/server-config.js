d6c2befa098bb31bd3bee656c2e7f0ae3fb2f331
Avinash Nishad <avinashn157@outlook.com>
2026-02-11 23:22:33 +0530

    fixed blueprint update not reflecting

--- main-backend/src/config/server-config.js
+++ main-backend/src/config/server-config.js
@@ -13,4 +13,15 @@ module.exports = {
     REDIS_HOST: process.env.REDIS_HOST || '127.0.0.1',
     REDIS_PORT: process.env.REDIS_PORT || 6379,
     REDIS_PASSWORD: process.env.REDIS_PASSWORD || undefined,
+    
+    // Email Config
+    SMTP_HOST: process.env.SMTP_HOST || 'smtp.gmail.com',
+    SMTP_PORT: process.env.SMTP_PORT || 587,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASS: process.env.SMTP_PASS,
+    SMTP_FROM: process.env.SMTP_FROM,
 };
+
+if (!process.env.SMTP_USER) {
+    console.warn('⚠️ SMTP_USER is missing in environment variables!');
+}
